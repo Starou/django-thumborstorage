b82167bdc233b866530157cc84eb7299c7f6570c
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-11-19 10:31:33 +0100

    test settings - set THUMBOR_WRITABLE_SERVER != THUMBOR_SERVER.

--- tests/settings.py
+++ tests/settings.py
@@ -7,7 +7,7 @@ DATABASES = {
 }
 
 ## Thumbor ##
-THUMBOR_SERVER = 'http://localhost:8888'
-THUMBOR_WRITABLE_SERVER = THUMBOR_SERVER
+THUMBOR_SERVER = 'http://ro.thumbor-server'
+THUMBOR_WRITABLE_SERVER = 'http://rw.thumbor-server'
 #THUMBOR_MEDIA_URL = 'http://localhost:8000/media'
 THUMBOR_SECURITY_KEY = 'MY_SECURE_KEY'

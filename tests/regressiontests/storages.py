a8145610088c3dde0ce59a13ba2dc2a50eef111b
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-11-19 11:03:38 +0100

    ThumborStorageFile.write() - add test.

--- tests/regressiontests/storages.py
+++ tests/regressiontests/storages.py
@@ -4,16 +4,16 @@ import mock
 import os
 import unittest
 
+CURRENT_DIR = os.path.abspath(os.path.split(__file__)[0])
+IMAGE_DIR = os.path.join(CURRENT_DIR, "..", "images")
 
 class MockedGetResponse:
-    _current_dir = os.path.abspath(os.path.split(__file__)[0])
-    _image_dir = os.path.join(_current_dir, "..", "images")
     status_code = 200
 
     def __init__(self, url):
         """Retrieve the file on the filesytem according to the name. """
         basename = os.path.basename(url)
-        filename = os.path.join(self._image_dir, basename)
+        filename = os.path.join(IMAGE_DIR, basename)
         self.content = open(filename, "r").read()
 
 
@@ -22,16 +22,30 @@ def mocked_thumbor_get_response(url):
     return response
 
 
+class MockedPostResponse:
+    headers = {}
+
+
+def mocked_thumbor_post_response(url, data, headers):
+    response = MockedPostResponse()
+    response.headers["location"] = "/image/oooooo32chars_random_idooooooooo/%s" % headers["Slug"]
+    return response
+
+
 class ThumborStorageFileTest(unittest.TestCase):
     def setUp(self):
         super(ThumborStorageFileTest, self).setUp()
         os.environ['DJANGO_SETTINGS_MODULE'] = "settings"
-        self.patcher = mock.patch('django_thumborstorage.storages.requests.get')
-        self.MockClass = self.patcher.start()
-        self.MockClass.side_effect = mocked_thumbor_get_response
+        self.patcher_get = mock.patch('django_thumborstorage.storages.requests.get')
+        self.MockGetClass = self.patcher_get.start()
+        self.MockGetClass.side_effect = mocked_thumbor_get_response
+
+        self.patcher_post = mock.patch('django_thumborstorage.storages.requests.post')
+        self.MockPostClass = self.patcher_post.start()
+        self.MockPostClass.side_effect = mocked_thumbor_post_response
 
     def tearDown(self):
-        self.patcher.stop()
+        self.patcher_get.stop()
 
     def test_get_file(self):
         from django_thumborstorage import storages
@@ -39,7 +53,7 @@ class ThumborStorageFileTest(unittest.TestCase):
         filename = '/image/5247a82854384f228c6fba432c67e6a8/people/new/TempletonPeck.jpg'
         thumbor_file = storages.ThumborStorageFile(filename, mode='r')
         thumbor_file.file
-        self.MockClass.called_with("%s%s" % (settings.THUMBOR_SERVER, filename))
+        self.MockGetClass.called_with("%s%s" % (settings.THUMBOR_SERVER, filename))
 
     def test_size(self):
         from django_thumborstorage import storages
@@ -47,8 +61,18 @@ class ThumborStorageFileTest(unittest.TestCase):
         filename = '/image/5247a82854384f228c6fba432c67e6a8/people/new/TempletonPeck.jpg'
         thumbor_file = storages.ThumborStorageFile(filename, mode='r')
         size = thumbor_file.size
-        self.MockClass.called_with("%s%s" % (settings.THUMBOR_SERVER, filename))
+        self.MockGetClass.called_with("%s%s" % (settings.THUMBOR_SERVER, filename))
         self.assertEqual(size, 9730)
+
+    def test_write(self):
+        from django_thumborstorage import storages
+        from django.conf import settings
+        from django.core.files.base import ContentFile
+        filename = 'people/HannibalSmith.jpg'
+        content = ContentFile(open('%s/HannibalSmith.jpg' % IMAGE_DIR))
+        thumbor_file = storages.ThumborStorageFile(filename, mode="w")
+        thumbor_file.write(content=content)
+        self.MockPostClass.called_with("%s/image" % settings.THUMBOR_WRITABLE_SERVER)
 
 
 class ThumborStorageTest(unittest.TestCase):

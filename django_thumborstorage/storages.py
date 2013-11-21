ed87ad5cc24313b6aa575cbc96962af886e2899e
Stanislas Guerra <stanislas.guerra@gmail.com>
2013-11-21 12:24:15 +0100

    delete-file: ThumborMigrationStorage.delete(): proxy-switch to ThumborStorage or FileSystemStorage.

--- django_thumborstorage/storages.py
+++ django_thumborstorage/storages.py
@@ -133,6 +133,12 @@ class ThumborMigrationStorage(ThumborStorage, FileSystemStorage):
         ThumborStorage.__init__(self, options)
         FileSystemStorage.__init__(self, location=location, base_url=base_url)
 
+    def delete(self, name):
+        if re.match(r"^/image/\w{32}/.*$", name):
+            return ThumborStorage.delete(self, name)
+        else:
+            return FileSystemStorage.delete(self, name)
+
     def url(self, name):
         if re.match(r"^/image/\w{32}/.*$", name):
             return ThumborStorage.url(self, name)

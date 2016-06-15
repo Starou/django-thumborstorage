d7aa3f4302214cf5e7c6879736efb7512b3f8180
Stanislas Guerra <sguerra@figarocms.fr>
2016-06-15 13:36:42 +0200

    issue_16_check_status_code_on_post: Version bump.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-thumborstorage',
-    version='0.92.0',
+    version='0.92.1',
     license='MIT Licence',
     author='Stanislas Guerra',
     author_email='stanislas.guerra@gmail.com',

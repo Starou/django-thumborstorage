8c21f126bdcf87f69a1069a02b4f408c7fa03fd9
Stanislas Guerra <sguerra@figarocms.fr>
2018-12-11 10:55:11 +0100

    fix-dependencies: Add libthumbor in dependencies.

--- setup.py
+++ setup.py
@@ -33,5 +33,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=['requests'],
+    install_requires=['requests', 'libthumbor'],
 )
